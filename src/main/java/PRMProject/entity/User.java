6cdfa59d0df9303cb9749a2c0a7768a388c3b6d1
minhthd <tanghoduyminh@gmail.com>
2019-11-17 22:43:52 +0700

    change database connection string

--- src/main/java/PRMProject/entity/User.java
+++ src/main/java/PRMProject/entity/User.java
@@ -18,8 +18,19 @@ public class User {
     @Id
     @Column(name = "USERNAME")
     private String username;
+
     @Column(name = "PASSWORD")
     private String password;
+
     @Column(name = "ROLE")
     private String role;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "PHONE")
+    private String phone;
+
+    @Column(name = "IS_DELETE")
+    private boolean isDelete;
 }
