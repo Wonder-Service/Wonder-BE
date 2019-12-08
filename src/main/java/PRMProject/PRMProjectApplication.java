f7b51c33db0a70ca98d574764545bed27dda6f51
minhthd <tanghoduyminh@gmail.com>
2019-12-08 15:06:02 +0700

    add global CORS config

--- src/main/java/PRMProject/PRMProjectApplication.java
+++ src/main/java/PRMProject/PRMProjectApplication.java
@@ -7,15 +7,18 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
@@ -27,40 +30,57 @@ import static springfox.documentation.builders.PathSelectors.regex;
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class PRMProjectApplication {
 
-	public static final String AUTHORIZATION_HEADER = "Authorization";
-	public static final String DEFAULT_INCLUDE_PATTERN = "/api/.*";
-	private ApiKey apiKey() {
-		return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
-	}
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String DEFAULT_INCLUDE_PATTERN = "/api/.*";
 
-	private SecurityContext securityContext() {
-		return SecurityContext.builder()
-				.securityReferences(defaultAuth())
-				.forPaths(regex(DEFAULT_INCLUDE_PATTERN))
-				.build();
-	}
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
 
-	List<SecurityReference> defaultAuth() {
-		AuthorizationScope authorizationScope
-				= new AuthorizationScope("global", "accessEverything");
-		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-		authorizationScopes[0] = authorizationScope;
-		return Lists.newArrayList(
-				new SecurityReference("JWT", authorizationScopes));
-	}
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(regex(DEFAULT_INCLUDE_PATTERN))
+                .build();
+    }
 
-	public static void main(String[] args) throws IOException {
-		SpringApplication.run(PRMProjectApplication.class, args);
-		InputStream serviceAccount = new ClassPathResource("firebasekey.json").getInputStream();
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(
+                new SecurityReference("JWT", authorizationScopes));
+    }
 
-		FirebaseOptions options = new FirebaseOptions.Builder()
-				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-				.setDatabaseUrl("https://fixxyworker.firebaseio.com")
-				.build();
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(PRMProjectApplication.class, args);
+        InputStream serviceAccount = new ClassPathResource("firebasekey.json").getInputStream();
 
-		FirebaseApp.initializeApp(options);
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setDatabaseUrl("https://fixxyworker.firebaseio.com")
+                .build();
 
-	}
+        FirebaseApp.initializeApp(options);
+
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*", "localhost:4200")
+                        .allowedMethods("*")
+                        .allowCredentials(false)
+                        .allowedHeaders("Authorization", "Content-Type", "Access-Control-Allow-Origin")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Access-Control-Allow-Headers", "Authorization", "Content-Type", "Access-Control-Allow-Origin");
+            }
+        };
+    }
 
 
 }
